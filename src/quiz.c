d33c1c492076011d62b33005ac719a9e0b7ac40e
schibe01 <schibe01@thu.de>
2026-06-23 11:12:42 +0200

    Insert circle and start of print circle, tail list

--- src/quiz.c
+++ src/quiz.c
@@ -2,3 +2,26 @@
 #include <stdlib.h>
 #include "quiz.h"
 
+void insert_question(QuestionList* list, Question* quest){
+
+    if(list->head == NULL){
+        list->head = quest;
+        list-> tail = quest;
+    }
+    else{
+        list->tail->next = quest;
+        list->tail = quest;
+        list->size += 1;
+    }
+
+}
+
+void print_question(QuestionList* list, int idx){
+
+    if(idx < 1|| idx > list->size){
+        fprintf(stderr, "Error: index doesn't extist\n");
+        return;
+    }
+
+    
+}
