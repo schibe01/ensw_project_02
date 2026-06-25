b6225a6320b2f54cc07f5ec4f541cbbfeb83e00b
Lucien Weißer <weisser.lucien@gmail.com>
2026-06-25 11:05:05 +0200

    Implemented delete_question and print_question_list

--- src/quiz.c
+++ src/quiz.c
@@ -13,7 +13,7 @@ void insert_question(QuestionList* list, Question* quest){
     list->tail = quest;
     quest->next = NULL;
     list->size++;
-    printf("%d\n", list->size);
+    printf("%zd\n", list->size);
 
 }
 
@@ -34,5 +34,52 @@ void print_question(QuestionList* list, int idx){
     }
     
     printf("\nWas ist Ihre Antwort?\n");    
+}
+
+void delete_question(QuestionList* list, int idx){
+    Question *ptr = list->head;
+    if(idx < 0 || idx >= list->size){
+        fprintf(stderr, "Error: index doesn't exist\n");
+        return;
+    }
+
+    if (idx == 0){
+        list -> head = ptr -> next;
+
+        free(ptr);
+
+        list -> size--;
+        return;
+    }
+
+    for(; idx-- -1; ){
+        ptr = ptr->next;
+    }
+
+    free(ptr -> next);
+
+    ptr -> next = ptr -> next -> next;
+
+    if(ptr -> next == NULL){
+        list -> tail = ptr;
+    }
+
+    list -> size--;
+}
+
+void print_question_list(QuestionList* list){
+    Question* ptr = list -> head -> next;
+    for (int i = 0 ; i < list -> size ; i++) {
+        printf("Question %d: ", i);
+        if (i == 0) {
+            printf("%s\n", list -> head -> quest);
+        } else if (i == list -> size - 1) {
+            printf("%s\n", list -> tail -> quest);
+        } else {
+            printf("%s\n", ptr -> quest);
+            ptr = ptr -> next;
+        }
+    }
+    printf("size: %zd\n\n", list -> size);
 }
 
